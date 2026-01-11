80c42098961d8dac5338a82f771c4792c551b083
DapinCi <dafinalfarizi001@gmail.com>
2026-01-12 00:01:55 +0700

    tantangan kedua

---
+++ function_composition.js
@@ -0,0 +1,28 @@
+/**
+ Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+ 
+ */
+
+var compose = function (functions) {
+  return function (x) {
+    if (functions.length === 0) {
+      return x;
+    } else {
+      let nilai = x;
+      for (let i = functions.length - 1; i >= 0; i--) {
+        nilai = functions[i](nilai);
+      }
+      return nilai;
+    }
+  };
+};
+
+const fn = compose([(x) => 10 * x, (x) => 10 * x, (x) => 10 * x]);
+console.log(fn(4));
