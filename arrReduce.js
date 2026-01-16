0c7f30e7f6e8a7bb25c3c335bf7f208980861d65
DapinCi <alfarizimuhammaddafin@gmail.com>
2026-01-16 14:06:18 +0700

    tantangan kesepuluh

---
+++ arrReduce.js
@@ -0,0 +1,17 @@
+var reduce = function (nums, fn, init) {
+  let nilai = init;
+  for (let i = 0; i < nums.length; i++) {
+    nilai = fn(nilai, nums[i]);
+  }
+  return nilai;
+};
+
+console.log(
+  reduce(
+    [1, 2, 3, 4],
+    function sum(accum, curr) {
+      return accum + curr * curr;
+    },
+    100
+  )
+);
